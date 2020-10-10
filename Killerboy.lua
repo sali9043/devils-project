fcb4c7ac89e5e1bb9be7ba493bac853bed64293f
sali9043 <sali9043@gmail.com>
2020-10-10 21:27:41 +0530

    Create Killerboy.lua

---
+++ Killerboy.lua
@@ -0,0 +1,17 @@
+function b1()
+ aa=gg.choice({
+   "حمايه العالميه v1 🇦🇪",
+   "حمايه الكوريه V2 🇦🇪",
+   "●▬▬▬●۩لعوده للخلف۩▬▬▬●nil,"Bypas - Killer PUBG")
+ if aa == 1 then byg() end
+ if aa == 2 then byg2() end
+ if aa == 3 then main() end
+ KPUBGM = 1
+ end
+
+function byg()
+ end
+ function byg2()
+ end
+
+b1()
