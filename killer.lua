599d1edb4e48e3561453c81fe2260cba8c3d9b84
sali9043 <sali9043@gmail.com>
2020-10-10 15:32:39 +0530

    Update killer.lua

--- killer.lua
+++ killer.lua
@@ -1,1 +1,23 @@
-gg.alert("successful")
+function b1()
+d1=gg.alert("Choose One Bypass and Play Which Work For you.","okay"
+if d1 == 1 then a1()end
+ KPUBGM = -1
+end
+
+fucntion a1()
+menu2 = gg.choice({
+menu2 = gg.choice({
+
+ function bypass3()
+ end
+
+while true do
+  if gg.isVisible(true) then
+KPUBGM = 1
+gg.setVisible(false)
+  end
+  gg.clearResults()
+  if KPUBGM == 1 then
+b1()
+  end
+end
